e1149c86b61d8a39696c035e89d529422a937cf5
Shubhang Kaushik <33983863+ShubhangPK@users.noreply.github.com>
2019-05-28 21:16:49 +0530

    Add files via upload

---
+++ Lex/Comment Line/input.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{
+int c,a,b;
+printf("Enter the value of a and b\n");
+scanf("%d%d",&a,&b);        
+/* Scan */
+printf("c = %d\n",c);
+//printf the value ofc
+
+}
